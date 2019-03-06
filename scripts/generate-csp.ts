93f30d5bf06badde1937cb755a4b9d2732b2196a
Gilad Peleg <giladp007@gmail.com>
2019-03-06 11:30:49 +0200

    Add github assets to allowed prefetch sources in CSP

--- scripts/generate-csp.ts
+++ scripts/generate-csp.ts
@@ -22,20 +22,23 @@ csp.addDirective(new CSP.ConnectSource().addValue(regularSourceDirective))
     .addDirective(new CSP.FontSource().addValue(extensiveSourceDirective))
     .addDirective(new CSP.FrameSource().addValue([CSP.PredefinedSource.Self]))
     .addDirective(
-        new CSP.ImageSource().addValue(regularSourceDirective.concat(CSP.SchemaSource.Data))
+        new CSP.ImageSource().addValue([...regularSourceDirective, CSP.SchemaSource.Data])
     )
     .addDirective(new CSP.MediaSource().addValue(localSourceDirective))
     .addDirective(new CSP.ObjectSource().addValue(CSP.PredefinedSource.None))
-    .addDirective(new CSP.PrefetchSource().addValue(regularSourceDirective))
+    .addDirective(new CSP.PrefetchSource().addValue([...regularSourceDirective, githubAssets]))
     .addDirective(
-        new CSP.ScriptSource().addValue(
-            regularSourceDirective.concat(CSP.PredefinedSource.UnsafeInline)
-        )
+        new CSP.ScriptSource().addValue([
+            ...regularSourceDirective,
+            CSP.PredefinedSource.UnsafeInline,
+        ])
     )
     .addDirective(
-        new CSP.StyleSource().addValue(
-            regularSourceDirective.concat(CSP.PredefinedSource.UnsafeInline, githubAssets)
-        )
+        new CSP.StyleSource().addValue([
+            ...regularSourceDirective,
+            CSP.PredefinedSource.UnsafeInline,
+            githubAssets,
+        ])
     )
     .addDirective(new CSP.WorkerSource().addValue(localSourceDirective))
     .addDirective(new CSP.ReportUri().setValue(reportUri))
