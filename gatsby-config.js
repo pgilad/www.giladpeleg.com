d9332a6833ce566b00c63770b224bde22ddde04f
Gilad Peleg <giladp007@gmail.com>
2019-03-06 10:22:44 +0200

    Fix code style and add support for emojis in markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,32 @@ module.exports = {
                             withWebp: true,
                         },
                     },
-                    'gatsby-remark-prismjs',
+                    {
+                        resolve: 'gatsby-remark-prismjs',
+                        options: {
+                            noInlineHighlight: false,
+                        },
+                    },
+                    {
+                        resolve: 'gatsby-remark-emojis',
+                        options: {
+                            // Deactivate the plugin globally (default: true)
+                            active: true,
+                            // Add a custom css class
+                            class: 'emoji-icon',
+                            // Select the size (available size: 16, 24, 32, 64)
+                            size: 64,
+                            // Add custom styles
+                            styles: {
+                                display: 'inline',
+                                margin: '0',
+                                'margin-top': '1px',
+                                position: 'relative',
+                                top: '5px',
+                                width: '25px',
+                            },
+                        },
+                    },
                     {
                         resolve: 'gatsby-remark-copy-linked-files',
                         options: {
