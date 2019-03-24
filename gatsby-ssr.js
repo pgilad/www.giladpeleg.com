bc56f5fb2c5c2ff1a74e40c0a2009e53172cfd05
Gilad Peleg <giladp007@gmail.com>
2019-03-24 15:52:29 +0200

    Add preconnect link to google analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+    setHeadComponents([
+        <link
+            rel="dns-prefetch"
+            key="dns-prefetch-google-analytics"
+            href="https://www.google-analytics.com"
+        />,
+    ]);
+};
