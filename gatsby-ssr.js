2c14a8f65abf22e4c073547d9e9c8aabc0f9b092
Gilad Peleg <giladp007@gmail.com>
2019-04-10 11:18:35 +0300

    Add more preconnect/dns-prefetch urls

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,14 +9,14 @@ import React from 'react';
 export const onRenderBody = ({ setHeadComponents }) => {
     setHeadComponents([
         <link
-            rel="preconnect"
-            key="preconnect-google-analytics"
-            href="https://www.google-analytics.com"
+            rel="preconnect dns-prefetch"
+            key="preconnect-google-marketingplatform"
+            href="https://marketingplatform.google.com"
         />,
         <link
-            rel="dns-prefetch"
-            key="dns-prefetch-google-analytics"
-            href="https://www.google-analytics.com"
+            rel="preconnect dns-prefetch"
+            key="preconnect-google"
+            href="https://www.google.com"
         />,
     ]);
 };
