5439006b90867a9f57671d2cbca02231ad865f3c
Gilad Peleg <giladp007@gmail.com>
2019-04-22 16:33:25 +0300

    Don't inline highlight with prism

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
                     {
                         resolve: 'gatsby-remark-prismjs',
                         options: {
-                            noInlineHighlight: false,
+                            noInlineHighlight: true,
                         },
                     },
                     {
