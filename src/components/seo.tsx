2538bb8de6116452a8a923301b48c39d76162e04
pgilad <gilad.peleg@forter.com>
2020-04-21 07:32:43 +0300

    Update all deps

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,7 +1,6 @@
 import { graphql, StaticQuery } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
-// import Helmet from 'react-helmet';
 
 import {
     getDescription,
