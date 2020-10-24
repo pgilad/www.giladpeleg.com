34f7944fb267f5a3bba3f5425b7dc9e3ccc23c60
pgilad <gilad.peleg@forter.com>
2020-10-24 16:00:08 +0300

    Add height and width to img icons on main page

--- src/components/home-right-overview.tsx
+++ src/components/home-right-overview.tsx
@@ -18,39 +18,79 @@ export const HomeRightOverview = (): JSX.Element => {
             <div className={styles.link}>
                 See my code:{" "}
                 <a href="https://github.com/pgilad">
-                    Github <img src={githubIcon} className={styles.linkIcon} alt="Github icon" />
+                    Github{" "}
+                    <img
+                        height="20"
+                        width="20"
+                        src={githubIcon}
+                        className={styles.linkIcon}
+                        alt="Github icon"
+                    />
                 </a>
             </div>
             <div className={styles.link}>
                 Follow me at:{" "}
                 <a href="https://twitter.com/GiladPeleg">
-                    Twitter <img src={twitterIcon} className={styles.linkIcon} alt="Twitter icon" />
+                    Twitter{" "}
+                    <img
+                        height="20"
+                        width="20"
+                        src={twitterIcon}
+                        className={styles.linkIcon}
+                        alt="Twitter icon"
+                    />
                 </a>
             </div>
             <div className={styles.link}>
                 See my resume:{" "}
                 <a href="https://www.linkedin.com/in/giladpeleg">
                     LinkedIn{" "}
-                    <img src={linkedinIcon} className={styles.linkIcon} alt="LinkedIn icon" />
+                    <img
+                        height="20"
+                        width="20"
+                        src={linkedinIcon}
+                        className={styles.linkIcon}
+                        alt="LinkedIn icon"
+                    />
                 </a>
             </div>
             <div className={styles.link}>
                 Send me a pigeon:{" "}
                 <a href="mailto:gilad@giladpeleg.com">
-                    Email <img src={emailIcon} className={styles.linkIcon} alt="Email icon" />
+                    Email{" "}
+                    <img
+                        height="20"
+                        width="20"
+                        src={emailIcon}
+                        className={styles.linkIcon}
+                        alt="Email icon"
+                    />
                 </a>
             </div>
             <div className={styles.link}>
                 Things I write about:{" "}
                 <Link to="/">
-                    Posts <img src={postsIcon} className={styles.linkIcon} alt="Posts icon" />
+                    Posts{" "}
+                    <img
+                        height="20"
+                        width="20"
+                        src={postsIcon}
+                        className={styles.linkIcon}
+                        alt="Posts icon"
+                    />
                 </Link>
             </div>
             <div className={styles.link}>
                 Follow this blog:{" "}
                 <a rel="alternate" type="application/rss+xml" href="/rss.xml">
                     RSS
-                    <img src={feedIcon} className={styles.linkIcon} alt="RSS Feed icon" />
+                    <img
+                        height="20"
+                        width="20"
+                        src={feedIcon}
+                        className={styles.linkIcon}
+                        alt="RSS Feed icon"
+                    />
                 </a>
             </div>
         </div>
